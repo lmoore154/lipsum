45509f57d11d1a9e5381d28815e72cefd82ee7c4
Laura Moore <rileyle@gmail.com>
2017-01-30 16:49:59 -0500

    Final edits

--- lipsum.rb
+++ lipsum.rb
@@ -8,17 +8,20 @@ num = ARGV[1] ? ARGV[1].to_i : 1
 
 quote = case lipsum_wanted
 when "Futurama"
-   "Kif, I have mated with a woman. Inform the men."
+   "Kif, I have mated with a woman. Inform the men. Why not create a National Endowment for Strip Clubs while we're at it? I barely knew Philip, but as a clergyman I have no problem telling his most intimate friends all about him."
 when "AD"
-   "That's why you always leave a note!"
+   "No! I was ashamed to be SEEN with you. I like being with you. I'm afraid I just blue myself. That's why you always leave a note!"
 when "Lorem"
    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 else
    "That's not a valid option. Try Futurama, AD, or Lorem!"
 end
 
-num.times do
-  puts quote
-end
+paragraphs = ""
+  num.times do
+    paragraphs = "#{quote}\n#{paragraphs}"
+  end
 
-system("echo \"#{quote}\" | pbcopy") #Thanks for the needed quote marks, David!
+puts paragraphs
+
+system("echo \"#{paragraphs}\" | pbcopy") #Thanks for the needed quote marks, David!
